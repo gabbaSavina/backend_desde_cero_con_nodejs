aa5b0ed977d2c8793ee242d931a235cc73c1a5d0
savinaGabba <gabbasavina@gmail.com>
2025-05-19 21:36:02 -0300

    se reorganisan las clases8a12 y proyectos integradores se crea

---
+++ I_fundamentos_de_javaScript/5_funciones/funciones_simples.js
@@ -0,0 +1,22 @@
+/*todos los casos en que se reciban parámetros, utilizar el prompt para el
+ingreso por consola de los valores.
+• Ejercicio 1:
+Crear una función que convierta pulgadas en centímetros. Recibe por
+parámetro pulgadas y retorna su equivalente en centímetros.
+• Ejercicio 2:
+Crear una función que recibe un string y lo convierte en una URL. Ej: “pepito”
+es devuelto como “http://www.pepito.com”
+• Ejercicio 3:
+Crear una función que recibe un string y devuelve la misma frase pero con
+admiración.
+• Ejercicio 4:
+Crear una función que calcule la edad de los perros, considerando que 1 año
+para nosotros son 7 de ellos.
+• Ejercicio 5:
+Crear una función que calcule el valor de tu hora de trabajo, introduciendo tu
+sueldo mensual como parámetro.
+PD: considerá que tu mes de trabajo tiene 40 horas.
+• Ejercicio 6:
+Crear la función calculadorIMC() que reciba la altura en metros y el peso en
+kilogramos y calcule el IMC de una persona. Luego, ejecutar la función
+probando diferentes valores.*/
